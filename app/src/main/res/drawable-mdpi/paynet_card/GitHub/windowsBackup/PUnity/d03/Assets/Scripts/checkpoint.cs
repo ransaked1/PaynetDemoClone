004035bb7491bb93b6e5d7176c6277487cb57126
root <root@LAPTOP-3LN59P5O.localdomain>
2020-10-14 14:03:04 +0100

    removed useless

--- app/src/main/res/drawable-mdpi/paynet_card/GitHub/windowsBackup/PUnity/d03/Assets/Scripts/checkpoint.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class checkpoint : MonoBehaviour {
-
-	public GameObject nextCheckpoint;
-	
-}
--- app/src/main/res/drawable-mdpi/paynet_card/GitHub/windowsBackup/PUnity/d03/Assets/Scripts/explosion.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class explosion : MonoBehaviour {
-	
-	void explosionEnd () {
-		Destroy (gameObject);
-	}
-}
